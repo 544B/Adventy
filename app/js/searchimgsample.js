326f6d25b08a7daa9516919cc52d0c15a1c699e9
yamato <yamato0024@hotmail.com>
2014-11-08 19:22:03 +0900

    Update searchimgsample.js
    あ

--- app/js/searchimgsample.js
+++ app/js/searchimgsample.js
@@ -4,7 +4,7 @@ var func;
 function searchImages(keyword,callback){
   $.ajax({
     type : 'GET',
-    url : 'http://v157-7-72-80.z1d10.static.cnode.jp:8080/imgsearch.php',
+    url : 'imgsearch.php',
       data : {keyword: keyword, limit: 4},
     dataType : 'json',
     success : _getPhotos
@@ -26,7 +26,7 @@ function _getPhotos(data){
 
 function callApi(day,callback){
   func = callback;
-  $.get("http://v157-7-72-80.z1d10.static.cnode.jp:8080/advent.php",{d: day},
+  $.get("advent.php",{d: day},
     function(data){
       var getdata = JSON.parse(data);
       quetion = getdata.question;
